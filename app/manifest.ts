7177e9924ed24445852ba5577f57e12c15394cfa
Jordi Hoven <hovenjordi@gmail.com>
2026-09-16 10:21:21 +0200

    update logo usage

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0070f3",
     icons: [
       {
-        src: "/icon-192.png",
+        src: "/logo-sm.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512.png",
+        src: "/logo-lg.png",
         sizes: "512x512",
         type: "image/png",
       },
